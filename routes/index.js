38a85c8acfe55431081bb52bb0a00574a8f58907
Unknown <mandersen+cf@riskalyze.com>
2018-10-25 16:26:28 -0700

    Change text again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express X' });
+  res.render('index', { title: 'Express Y' });
 });
 
 module.exports = router;
